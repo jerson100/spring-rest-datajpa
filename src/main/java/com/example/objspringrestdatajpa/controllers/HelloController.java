060a66b370703401e66bc0db021bb4bc7e9fd1d4
jerson100 <juamkoo@gmail.com>
2022-07-26 12:49:58 -0500

    Agregando modo

--- src/main/java/com/example/objspringrestdatajpa/controllers/HelloController.java
+++ src/main/java/com/example/objspringrestdatajpa/controllers/HelloController.java
@@ -1,13 +1,16 @@
 package com.example.objspringrestdatajpa.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @Value("${app.envexample}")
+    private String message;
     @GetMapping(path = "/helloworld")
     public String helloWorld(){
-        return "Hello World replicando.";
+        return message;
     }
     @GetMapping(path = "/html")
     public String getHtml(){
